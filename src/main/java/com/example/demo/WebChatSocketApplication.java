9a7c58d21d43caf830694623cbbd90ca110fd091
*cRc4lRai4f <truongvonhat204@gmail.com>
2020-06-06 02:13:49 +0700

    first commit

---
+++ src/main/java/com/example/demo/WebChatSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatSocketApplication.class, args);
+	}
+
+}
